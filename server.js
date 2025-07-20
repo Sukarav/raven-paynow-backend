507dd387518991ce47c6591c301a7c3baa0f156a
Sukarav <sukaravtech@gmail.com>
2025-07-20 19:07:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
